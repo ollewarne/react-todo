eb68a62ed7bd4b26ce98c9982a1d0c176c40072a
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-01-18 00:09:26 +0100

    basic add/remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    function removeTodo(toRemove) {
+        const newTodos = todos.filter(todo => todo !== toRemove)
+        setTodos(newTodos)
+    }
+
     return (
-        <div>hello world</div>
+        <>
+            <ul className='todoList' onClick={(e) => {removeTodo(e.target.textContent) }}>
+                {
+                    todos.filter(todo => todo).map(todo => <li key={todo}>{todo}</li>)
+                }
+            </ul>
+            <input type="text" onChange={(e) => {setInputValue(e.target.value)}} />
+            <button onClick={() => setTodos([...todos, inputValue])}>add todo item</button>
+        </>
     )
 }
 
